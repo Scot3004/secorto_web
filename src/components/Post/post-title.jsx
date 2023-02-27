4c49ebdc1caaac7c630743843426cd97f366fa2c
Sergio C. Orozco Torres <scot3004@gmail.com>
2023-02-26 23:12:42 -0500

    Configuración a11y eslint

--- src/components/Post/post-title.jsx
+++ src/components/Post/post-title.jsx
@@ -2,7 +2,6 @@
 import { jsx } from "theme-ui"
 import { Themed } from "@theme-ui/mdx"
 
-// eslint-disable-next-line react/jsx-pascal-case
 const PostTitle = props => <Themed.h1 sx={{ color: "primary" }} {...props} />
 
 export default PostTitle
