7ee80624773f5b343ce3581bb0679dad93fd1440
Sergio C. Orozco Torres <scot3004@gmail.com>
2023-08-30 23:29:40 -0500

    Separados stubs en un archivo aparte

--- cypress/e2e/accessibility.test.ts
+++ cypress/e2e/accessibility.test.ts
@@ -1,20 +1,8 @@
+import { darkMode, lightMode } from "./stubs"
+
 describe("Accessibility tests Dark mode", () => {
   beforeEach(() => {
-      cy.visit("/", {
-        onBeforeLoad (win) {
-          const callback = cy.stub(win, 'matchMedia')
-          callback.withArgs('(prefers-color-scheme: dark)')
-          .as("dark-mode")
-          .returns({
-            matches: true,
-          })
-          callback
-          .as("all media")
-          .returns({
-            matches: true,
-          })
-        },
-      }).get("main")
+      cy.visit("/", darkMode()).get("main")
       cy.injectAxe()
   })
   it("Has no detectable accessibility violations on load", () => {
@@ -24,21 +12,7 @@ describe("Accessibility tests Dark mode", () => {
 
 describe("Accessibility tests light mode", () => {
   beforeEach(() => {
-      cy.visit("/", {
-        onBeforeLoad (win) {
-          const callback = cy.stub(win, 'matchMedia')
-          callback.withArgs('(prefers-color-scheme: dark)')
-          .as("no dark-mode")
-          .returns({
-            matches: false,
-          })
-          callback
-          .as("all media")
-          .returns({
-            matches: true,
-          })
-        },
-      }).get("main")
+      cy.visit("/", lightMode()).get("main")
       cy.injectAxe()
   })
   it("Has no detectable accessibility violations on load", () => {
